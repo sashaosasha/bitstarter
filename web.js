291f468b16889fb08e97cf27cce13a3c4452fd13
Sasha Ognev <sashaosasha@gmail.com>
2013-07-09 23:37:03 +0000

    typo

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var txt = fs.readFileSync('index.html').toString();
   response.send(txt);
-}
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
